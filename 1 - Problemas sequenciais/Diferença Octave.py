0676a03d492c447c46b363283e51b1d024dbe274
Estevão Holanda Lima Junior <129328766+estevaohljunior@users.noreply.github.com>
2023-03-29 22:00:52 -0300

    Exercícios

---
+++ 1 - Problemas sequenciais/Diferença Octave.py
@@ -0,0 +1,5 @@
+A = int(input("DIGITE O VALOR A:\n"));
+B = int(input("DIGITE O VALOR B:\n"));
+C = int(input("DIGITE O VALOR C:\n"));
+D = int(input("DIGITE O VALOR D:\n"));
+print("DIFERENCA =",(A*B)-(C*D))
